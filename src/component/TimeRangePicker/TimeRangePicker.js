8ff203b8ea8fd0ab87eb06e62faff880ba32bc8e
SING <singsytsang@gmail.com>
2024-06-01 16:02:41 +0800

    .

--- src/component/TimeRangePicker/TimeRangePicker.js
+++ src/component/TimeRangePicker/TimeRangePicker.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { scaleTime } from 'd3-scale'
 import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider'
 import moment from 'moment'
+import { MdOutlineZoomIn, MdOutlineZoomOut, MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 import SliderRail from './components/SliderRail'
 import Track from './components/Track'
@@ -55,6 +56,7 @@ const TimeRangePicker = ({
   containerClassName,
   onUpdateCallback,
   onChangeCallback,
+  setTimelineInterval,
   selectedInterval,
   timelineInterval,
   sliderRailClassName,
@@ -66,13 +68,77 @@ const TimeRangePicker = ({
   error,
   mode
 }) => {
+  const [selectedScale, setSelectedScale] = useState(0)
+  const scale = [24, 12, 8, 4, 2]
+
   const formattedDisabledIntervals = getFormattedBlockedIntervals(disabledIntervals, timelineInterval)
   
+  useEffect(() => {
+    const isValuesNotValid = checkIsValid(selectedInterval)
+
+    onUpdateCallback({ error: isValuesNotValid})
+  }, [formattedDisabledIntervals])
+
+  const zoomIn = () => {
+    if (selectedScale == scale.length-1 ) {
+      return
+    }
+
+    setSelectedScale(selectedScale+1)
+    scaleUpdate(selectedScale+1)
+  }
+
+  const zoomOut = () => {
+    if (selectedScale == 0) {
+      return
+    }
+
+    setSelectedScale(selectedScale-1)
+    scaleUpdate(selectedScale-1)
+  }
+
+  const shiftLeft = () => {
+    const curr_start = timelineInterval[0]
+
+    const new_start = moment.max(
+      moment(curr_start).subtract(scale[selectedScale]/2, 'hours'),
+      moment(curr_start).startOf('day')
+    )
+
+    setTimelineInterval([addHour(moment(new_start), 0), addHour(moment(new_start), scale[selectedScale])])
+  }
+
+  const shiftRight = () => {
+    const curr_end = timelineInterval[1]
+
+    const new_end = moment.min(
+      moment(curr_end).add(scale[selectedScale]/2, 'hours'),
+      moment(curr_end).endOf('day')
+    )
+
+    setTimelineInterval([addHour(new_end, (scale[selectedScale])*-1), addHour(moment(new_end), 0)])
+  }
+
+  const scaleUpdate = (value) => {
+    const newScale = scale[value]
+
+    const curr_start = timelineInterval[0]
+    const curr_end = timelineInterval[1]
+    const center = (Math.abs(moment(curr_start).diff(curr_end)))/2+Math.min(moment(curr_start).valueOf(),moment(curr_end).valueOf())
+    
+    setTimelineInterval([addHour(center, (newScale/2)*-1), addHour(center, newScale/2)])
+  }
+
+  const addHour = (value, diff) => {
+    return moment(value).add(diff, 'hours').startOf('hour').toDate()
+  }
+
   const now = () => {
     return getNowConfig(timelineInterval)
   }
 
   const onChange = (newTime) => {
+    console.log(newTime)
     const formattedNewTime = newTime.map(t => new Date(t))
     onChangeCallback(formattedNewTime)
   }
@@ -91,11 +157,21 @@ const TimeRangePicker = ({
     return isStartInBlockedInterval || isEndInBlockedInterval
   }
 
-  const onUpdate = (newTime) => {
+  const checkIsValid = (newTime) => {
     if (formattedDisabledIntervals != null && formattedDisabledIntervals.length) {
       const isValuesNotValid = formattedDisabledIntervals.some(({ source, target }) =>
-        checkIsSelectedIntervalNotValid(newTime, source, target))
+        checkIsSelectedIntervalNotValid(newTime, source, target)
+      )
+
+      return isValuesNotValid
+    }
+  }
+
+  const onUpdate = (newTime) => {
+    if (formattedDisabledIntervals != null && formattedDisabledIntervals.length) {
+      const isValuesNotValid = checkIsValid(newTime)
       const formattedNewTime = newTime.map(t => new Date(t))
+
       onUpdateCallback({ error: isValuesNotValid, time: formattedNewTime })
       return
     }
@@ -111,101 +187,111 @@ const TimeRangePicker = ({
   const domain = timelineInterval.map(t => Number(t))
 
   return (
-    <div className={containerClassName || 'react_time_range__time_range_container' }>
-      <Slider
-        mode={mode}
-        step={step}
-        domain={domain}
-        onUpdate={onUpdate}
-        onChange={onChange}
-        values={selectedInterval.map(t => +t)}
-        rootStyle={{ position: 'relative', width: '100%' }}
-      >
-        <Rail>
-          {({ getRailProps }) =>
-            <SliderRail className={sliderRailClassName} getRailProps={getRailProps} />}
-        </Rail>
+    <div>
+      <div style={{"display": "flex", "align-items": "center", "paddingLeft": "30px", "paddingRight": "30px"}}>
+        <MdOutlineZoomIn size={25} onClick={() => zoomIn()}/>
+        <MdOutlineZoomOut size={25} onClick={() => zoomOut()}/>
 
-        <Handles>
-          {({ handles, getHandleProps }) => (
-            <div>
-              {handles.map(handle => (
-                <Handle
-                  error={error}
-                  key={handle.id}
-                  handle={handle}
-                  domain={domain}
-                  getHandleProps={getHandleProps}
-                />
-              ))}
-            </div>
-          )}
-        </Handles>
+        <MdKeyboardArrowLeft size={25} onClick={() => shiftLeft()}/>
+        <MdKeyboardArrowRight size={25} onClick={() => shiftRight()}/>
+      </div>
+      
+      <div className={containerClassName || 'react_time_range__time_range_container' }>
+        <Slider
+          mode={mode}
+          step={step}
+          domain={domain}
+          onUpdate={onUpdate}
+          onChange={onChange}
+          values={[moment.max(moment(selectedInterval[0]), moment(timelineInterval[0])), moment.min(moment(selectedInterval[1]), moment(timelineInterval[1]))].map(t => +t)}
+          rootStyle={{ position: 'relative', width: '100%' }}
+        >
+          <Rail>
+            {({ getRailProps }) =>
+              <SliderRail className={sliderRailClassName} getRailProps={getRailProps} />}
+          </Rail>
 
-        <Tracks left={false} right={false}>
-          {({ tracks, getTrackProps }) => (
-            <div>
-              {tracks.map(({ id, source, target }) =>
-                <Track
-                  error={error}
-                  key={id}
-                  source={source}
-                  target={target}
-                  getTrackProps={getTrackProps}
-                />
-              )}
-            </div>
-          )}
-        </Tracks>
-
-        {formattedDisabledIntervals != null && formattedDisabledIntervals.length && (
-          <Tracks left={false} right={false}>
-            {({ getTrackProps }) => (
+          <Handles>
+            {({ handles, getHandleProps }) => (
               <div>
-                {formattedDisabledIntervals.map(({ id, source, target }) => (
-                  <Track
-                    key={id}
-                    source={source}
-                    target={target}
-                    getTrackProps={getTrackProps}
-                    disabled
+                {handles.map(handle => (
+                  <Handle
+                    error={error}
+                    key={handle.id}
+                    handle={handle}
+                    domain={domain}
+                    getHandleProps={getHandleProps}
                   />
                 ))}
               </div>
             )}
-          </Tracks>
-        )}
+          </Handles>
 
-        {showNow && (
           <Tracks left={false} right={false}>
-            {({ getTrackProps }) => (
-              <Track
-                key={now.id}
-                source={now.source}
-                target={now.target}
-                getTrackProps={getTrackProps}
-              />
+            {({ tracks, getTrackProps }) => (
+              <div>
+                {tracks.map(({ id, source, target }) =>
+                  <Track
+                    error={error}
+                    key={id}
+                    source={source}
+                    target={target}
+                    getTrackProps={getTrackProps}
+                  />
+                )}
+              </div>
             )}
           </Tracks>
-        )}
 
-        <Ticks values={getDateTicks()}>
-          {({ ticks }) => (
-            <div>
-              {ticks.map(tick => (
-                <Tick
-                  key={tick.id}
-                  tick={tick}
-                  count={ticks.length}
-                  format={formatTick}
-                />
-              ))}
-            </div>
+          {formattedDisabledIntervals != null && formattedDisabledIntervals.length && (
+            <Tracks left={false} right={false}>
+              {({ getTrackProps }) => (
+                <div>
+                  {formattedDisabledIntervals.map(({ id, source, target }) => (
+                    <Track
+                      key={id}
+                      source={source}
+                      target={target}
+                      getTrackProps={getTrackProps}
+                      disabled
+                    />
+                  ))}
+                </div>
+              )}
+            </Tracks>
           )}
-        </Ticks>
-      </Slider>
-    </div>
-  )
+
+          {showNow && (
+            <Tracks left={false} right={false}>
+              {({ getTrackProps }) => (
+                <Track
+                  key={now.id}
+                  source={now.source}
+                  target={now.target}
+                  getTrackProps={getTrackProps}
+                />
+              )}
+            </Tracks>
+          )}
+
+          <Ticks values={getDateTicks()}>
+            {({ ticks }) => (
+              <div>
+                {ticks.map(tick => (
+                  <Tick
+                    key={tick.id}
+                    tick={tick}
+                    count={ticks.length}
+                    format={formatTick}
+                  />
+                ))}
+              </div>
+            )}
+          </Ticks>
+        </Slider>
+      </div>
+  </div>
+)
 }
 
 TimeRangePicker.propTypes = {
@@ -230,4 +316,4 @@ TimeRangePicker.defaultProps = {
   mode: 3,
 }
 
-export { TimeRangePicker }
+export default TimeRangePicker;
